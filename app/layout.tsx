3ce8be81452142579384a6647f6741488cc469e7
chairich <33069090+chairich@users.noreply.github.com>
2025-07-17 00:25:17 +0700

    🛠️ Add manifest to open petbidthai.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,3 @@
-export const metadata = {
-  title: 'PetBidThai',
-  description: 'PWA Auction platform',
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="theme-color" content="#003399" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-}
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#003399" />
+<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
