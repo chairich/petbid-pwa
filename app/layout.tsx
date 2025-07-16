115d42885550d412bb3ba4c25a16d4a337d262c9
chairich <33069090+chairich@users.noreply.github.com>
2025-07-17 00:49:34 +0700

    🛠️ Add manifest to open petbidthai.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,24 @@
-export const metadata = {
+// app/layout.tsx
+import './globals.css'
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
   title: 'PetBid Thai',
-  description: 'PWA for PetBid',
-  manifest: '/manifest.json',
+  description: 'PWA for PetBidThai',
   themeColor: '#003399',
-};
+  manifest: '/manifest.json',
+  icons: {
+    icon: '/icons/icon-192x192.png',
+    apple: '/icons/icon-192x192.png',
+  },
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="th">
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
